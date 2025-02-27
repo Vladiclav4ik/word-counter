75ec6bcf68e51ce30b20cdfc479a93443d9772da
N3k14 <nikita.belik.2020@mail.ru>
2025-02-27 13:53:34 +0000

    add tests

---
+++ cicd/test_word_counter.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "word_counter.h"
+
+TEST(WordCounterTest, EmptyString) {
+    EXPECT_EQ(countWords(""), 0);
+}
+
+TEST(WordCounterTest, SingleWord) {
+    EXPECT_EQ(countWords("Hello"), 1);
+}
+
+TEST(WordCounterTest, MultipleWords) {
+    EXPECT_EQ(countWords("Hello world!"), 2);
+}
+
+TEST(WordCounterTest, ExtraSpaces) {
+    EXPECT_EQ(countWords("  This  is   a test   "), 4);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
