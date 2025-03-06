aa1a5c841b57a5a1d9b95e5719350d7eb8ebe122
N3k14 <nikita.belik.2020@mail.ru>
2025-03-06 13:20:10 +0000

    add parameter check

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include "word_counter.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+    if (argc > 1 && std::string(argv[1] == "--success") {
+        std::cout << "Success docker launch, exiting with code 0." << std::endl; 
+    }
+    
     std::string input;
     std::cout << "Введите строку:\n";
     std::getline(std::cin, input);
