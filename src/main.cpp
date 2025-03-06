f285adf87ead857e2af27ab6df89ec6889d02a39
N3k14 <nikita.belik.2020@mail.ru>
2025-03-06 13:36:06 +0000

    add forced exit after parameter check

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 int main(int argc, char* argv[]) {
     if (argc > 1 && std::string(argv[1]) == "--success") {
         std::cout << "Success docker launch, exiting with code 0." << std::endl; 
+        return 0;
     }
     
     std::string input;
